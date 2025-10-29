56880ba8a26435cedddde19a7e71165eb2181c71
Your Name <Nephi.Allen@students.snow.edu>
2025-10-29 09:54:57 -0600

    done with c6 challenges one and two

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,8 +1,37 @@
-﻿
-Console.Clear();
+﻿//assignments c6#1 and c6#2
+
+//challenge 1: "The Variable Shop"
+int one = 1;
+uint two = 2;
+byte three = 3;
+short four = 4;
+long five = 5;
+sbyte six = 6;
+ushort seven = 7;
+ulong eight = 8;
+float nine = 9;
+double ten = 10;
+decimal eleven = 11;
+bool twelve = true;
+Console.WriteLine(@$"Hello! this program needs to have one of each type of variable, totaling twelve variables.: 
+{one}
+{two}
+{three}
+{four}
+{five}
+{six}
+{seven}
+{eight}
+{nine}
+{ten}
+{eleven}
+12
+{twelve}
+");
 
 
-
+//challenge 2: "The variable shop returns"
+// uh... i did that on the first one... i guess it's done!😊
 
 
 
